4f5512ec27250eb8afc710095f07563c55f819cc
dltkd1395 <dltkd1395@gmail.com>
2022-09-27 15:53:35 +0900

    feat: Multi Module 추가

---
+++ presenation/src/main/java/com/qure/presenation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.qure.presenation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.quere.presenation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
