a315bc929bdd56c4f64b4f05666613948d45cd38
dltkd1395 <dltkd1395@gmail.com>
2022-09-27 22:24:35 +0900

    feat: Base Activity, Base Fragment 추가

---
+++ presenation/src/main/java/com/quere/presenation/base/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.quere.presenation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T: ViewDataBinding>(@LayoutRes val layoutResInt : Int) : Fragment()  {
+
+    lateinit var binding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, layoutResInt, container, false)
+
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+
+        binding.lifecycleOwner = this
+
+    }
+
+    abstract fun initView()
+
+
+
+}
