4bf45630604bcc39f429ae66b37c397c2a04d6d8
dltkd1395 <dltkd1395@gmail.com>
2022-09-28 01:22:11 +0900

    feat: AppConstants 추가

---
+++ presenation/src/main/java/com/quere/presenation/AppConstants.kt
@@ -0,0 +1,30 @@
+package com.quere.presenation
+
+class AppConstants {
+
+    companion object {
+
+        const val UPCOMING = "upcoming"
+        const val POPULAR = "popular"
+        const val TOP_RATED = "top_rated"
+        const val NOW_PLAYING = "now_playing"
+        const val ONTHEAIR = "on_the_air"
+
+        const val SIMILAR = "similar"
+        const val RECOMMEND = "recommend"
+
+        const val MOVIE = "movie"
+        const val TV = "tv"
+
+        const val ACTION = "28"
+        const val FANTASY = "10751"
+        const val ANIMATION = "16"
+        const val COMEDY = "35"
+        const val ROMANCE = "10749"
+        const val CRIME = "80"
+        const val MYSTERY = "9648"
+        const val HORROR = "27"
+        const val MUSIC = "10402"
+
+    }
+}
